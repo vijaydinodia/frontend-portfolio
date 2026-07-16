0d04d5107de82e6a3a2767a83ee273d137d795d5
vijay <vijaydinodia548@gmail.com>
2026-07-16 12:26:59 +0530

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 import Base_URL from './api'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 
 // Set global base URL for all axios requests
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
